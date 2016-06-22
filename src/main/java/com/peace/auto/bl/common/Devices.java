51eabb52c7edfc9ab1f1d0bfaa899db2b3ba1a54
peace0phmind <peace0phmind@gmail.com>
2016-06-22 16:57:29 +0800

    fix bug

--- src/main/java/com/peace/auto/bl/common/Devices.java
+++ src/main/java/com/peace/auto/bl/common/Devices.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 @Data
 public class Devices {
 //    public static final Device DEVICE = new Device("efc444e7-aeb9-4ce4-8993-9e777ed033d9", "Samsung Galaxy S2 - 4.1.1 - API 16 - 480x800_1");
-    public static final Device DEVICE_1 = new Device("592b398d-99c7-4949-a39b-80ff044f585d", "device1");
+    public static final Device DEVICE_1 = new Device("70cb6caa-fe3d-4d10-b778-a1b06c4fe677", "device1");
     public static final Device DEVICE_2 = new Device("d11a2220-e8b9-480e-851e-b62e6953dadc", "device2");
     public static final Device DEVICE_3 = new Device("8e415fa8-fabd-4429-9c62-4ee3a6b3e9b3", "device3");
     public static final Status status = new Status();
