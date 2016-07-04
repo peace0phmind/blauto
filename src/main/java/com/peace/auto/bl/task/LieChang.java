825aa3b1447db64505e7f57fc83517fb3c9add82
peace0phmind <peace0phmind@gmail.com>
2016-07-04 13:49:09 +0800

    fix bug

--- src/main/java/com/peace/auto/bl/task/LieChang.java
+++ src/main/java/com/peace/auto/bl/task/LieChang.java
@@ -6,6 +6,7 @@ import com.peace.auto.bl.Task;
 import lombok.extern.slf4j.Slf4j;
 import org.sikuli.script.FindFailed;
 import org.sikuli.script.Match;
+import org.sikuli.script.Pattern;
 import org.sikuli.script.Region;
 
 import java.util.List;
@@ -73,8 +74,11 @@ public class LieChang implements IDo {
 
                         Thread.sleep(1000L);
 
-                        Match baopokuangren = region.exists(baseDir + "baopokuangren.png");
-                        if (baopokuangren != null && baopokuangren.getScore() > 0.95) {
+                        region.saveScreenCapture(".", "ttt-" + status.getCurrentUser());
+                        log.info("save image ok.");
+
+                        Match peacemianfei = region.exists(new Pattern(baseDir + "peacemianfei.png").similar(0.95f));
+                        if (peacemianfei != null) {
                             region.click(baseDir + "kaishizhandou.png");
 
                             Match dianjijixu = region.exists(baseDir + "dianjipingmujixu.png", 10);
