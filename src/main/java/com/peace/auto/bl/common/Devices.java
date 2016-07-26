3ff8159cb05c30e91a3a93bcf2991e42ddcb1570
peace0phmind <peace0phmind@gmail.com>
2016-07-26 21:45:46 +0800

    fix bug

--- src/main/java/com/peace/auto/bl/common/Devices.java
+++ src/main/java/com/peace/auto/bl/common/Devices.java
@@ -11,9 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Data
 public class Devices {
-    public static final Device DEVICE_1 = new Device("d8653e08-1255-4b69-926e-df05431736ec", "Samsung Galaxy S2 - 4.1.1 - API 16 - 480x800");
-    public static final Device DEVICE_2 = new Device("e6faded0-f5da-4ce2-ac17-f6279b24d494", "Samsung Galaxy S2 - 4.1.1 - API 16 - 480x800_1");
-    public static final Device DEVICE_3 = new Device("d0ed3766-7b64-4810-b0e3-553f8d9d1bd3", "Samsung Galaxy S2 - 4.1.1 - API 16 - 480x800_2");
+    public static final Device DEVICE_1 = new Device("b998de4e-3484-4da8-9433-e5606ccf7b75", "Samsung Galaxy S2 - 4.1.1 - API 16 - 480x800");
+    public static final Device DEVICE_2 = new Device("4ff0d053-278f-4094-b2b6-101c6b93bd90", "Samsung Galaxy S2 - 4.1.1 - API 16 - 480x800_1");
+    public static final Device DEVICE_3 = new Device("2727b492-5d76-4f1a-99ab-95e43f319b17", "Samsung Galaxy S2 - 4.1.1 - API 16 - 480x800_2");
     public static final Status status = new Status();
     public static final DengLu DENG_LU = new DengLu();
 }
