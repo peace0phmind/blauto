6751cf5b5256e25dcbfbc767e7c227e1636cfae2
peace0phmind <peace0phmind@gmail.com>
2016-08-07 13:21:57 +0800

    change device id

--- src/main/java/com/peace/auto/bl/common/Devices.java
+++ src/main/java/com/peace/auto/bl/common/Devices.java
@@ -11,7 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Data
 public class Devices {
-    public static final Device DEVICE_1 = new Device("b998de4e-3484-4da8-9433-e5606ccf7b75", "Samsung Galaxy S2 - 4.1.1 - API 16 - 480x800");
+    public static final Device DEVICE_1 = new Device("40b6353b-2e5a-4894-b5e6-b9f6b645a513", "Samsung Galaxy S2 - 4.1.1 - API 16 - 480x800");
     public static final Device DEVICE_2 = new Device("4ff0d053-278f-4094-b2b6-101c6b93bd90", "Samsung Galaxy S2 - 4.1.1 - API 16 - 480x800_1");
     public static final Device DEVICE_3 = new Device("2727b492-5d76-4f1a-99ab-95e43f319b17", "Samsung Galaxy S2 - 4.1.1 - API 16 - 480x800_2");
     public static final Status status = new Status();
